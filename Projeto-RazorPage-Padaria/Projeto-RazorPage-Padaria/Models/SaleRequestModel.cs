d151d80e50649b8e76156deeef5bc7d1f8712bca
Gabriel Fernandes <114429621+GabrielFer555@users.noreply.github.com>
2024-09-11 19:51:43 -0300

    continuado trabalho

---
+++ Projeto-RazorPage-Padaria/Projeto-RazorPage-Padaria/Models/SaleRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Projeto_RazorPage_Padaria.Models
+{
+    public class SaleRequestModel
+    {
+        public int Id { get; set; }
+        public List<SalesItem> SalesItems { get; set; }
+    }
+}
